bc27f3bbebb6d5aab042957521973560be52e35e
mkcomer237 <mkcomer@gmail.com>
2021-09-24 08:50:54 -0700

    Build intial ingestor abstract base class

---
+++ QuoteEngine/IngestorInterface.py
@@ -0,0 +1,27 @@
+"""Create a base class for ingesting files."""
+
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class IngestorInterface():
+    """Base ingestor class.
+
+    This contains class methods and does not need to be
+    instantiated. It encapsulates our import functionality
+    and can be called to load different file types.
+    """
+
+    allowed_extensions = []
+
+    @classmethod
+    def can_ingest(cls, path):
+        """Check that the file extension is in the allow list."""
+        return path.split('.')[-1] in cls.allowed_extensions
+
+    @abstractmethod
+    @classmethod
+    def parse_quotes(cls, path: str) -> List(QuoteModel):
+        """Extract quotations line by line from the document."""
+        pass
